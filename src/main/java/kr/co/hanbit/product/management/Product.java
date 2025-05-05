739e8d72d9cf9666f8b51115ced43ea269ea9d88
kmh990303 <labcs@naver.com>
2025-05-05 15:14:22 +0900

    초기 프로젝트 구축

---
+++ src/main/java/kr/co/hanbit/product/management/Product.java
@@ -0,0 +1,42 @@
+package kr.co.hanbit.product.management;
+
+public class Product {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    private String name;
+    private Integer price;
+    private Integer amount;
+}
