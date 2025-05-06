0a3de6fd0591ec435c07802a94b4f3238ab99915
kmh990303 <labcs@naver.com>
2025-05-06 15:12:46 +0900

    레이어드 아키텍쳐 적용

---
+++ src/main/java/kr/co/hanbit/product/management/application/SimpleProductService.java
@@ -0,0 +1,38 @@
+package kr.co.hanbit.product.management.application;
+
+import kr.co.hanbit.product.management.domain.Product;
+import kr.co.hanbit.product.management.presentation.ProductDto;
+import kr.co.hanbit.product.management.infrastructure.ListProductRepository;
+import kr.co.hanbit.product.management.presentation.ProductDto;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SimpleProductService {
+
+    private ListProductRepository listProductRepository;
+    private ModelMapper modelMapper;
+
+    @Autowired
+    SimpleProductService(ListProductRepository listProductRepository, ModelMapper modelMapper) {
+        this.listProductRepository = listProductRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    public ProductDto add(ProductDto productDto) {
+        Product product = modelMapper.map(productDto, Product.class);
+
+        Product savedProduct = listProductRepository.add(product);
+
+        ProductDto savedProductDto = modelMapper.map(savedProduct, ProductDto.class);
+
+        return savedProductDto;
+    }
+
+    public ProductDto findById(Long id) {
+        Product product = listProductRepository.findById(id);
+        ProductDto productDto = modelMapper.map(product, ProductDto.class);
+        return productDto;
+    }
+}
---
+++ src/main/java/kr/co/hanbit/product/management/infrastructure/ListProductRepository.java
@@ -0,0 +1,29 @@
+package kr.co.hanbit.product.management.infrastructure;
+
+import kr.co.hanbit.product.management.domain.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class ListProductRepository {
+
+    private List<Product> products = new CopyOnWriteArrayList<>();
+    private AtomicLong sequence = new AtomicLong(1);
+
+    public Product add(Product product) {
+        product.setId(sequence.getAndAdd(1L));
+
+        products.add(product);
+        return product;
+    }
+
+    public Product findById(Long id) {
+        return products.stream()
+                .filter(product -> product.sameId(id))
+                .findFirst()
+                .orElseThrow();
+    }
+}
